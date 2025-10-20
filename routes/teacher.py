1134fbde5e88bcab438c015c908cc7f496476837
salmahaz <123059446+salmahaz@users.noreply.github.com>
2025-10-20 11:43:20 +0300

    Refactor import statements in teacher.py
    Updated import statements to remove relative paths.

--- routes/teacher.py
+++ routes/teacher.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request, jsonify
-from ..db import SessionLocal
-from ..models import Teacher, Session
-from ..utils import hash_password, verify_password, generate_code
-from ..sockets import socketio
+from db import SessionLocal
+from models import Teacher, Session
+from utils import hash_password, verify_password, generate_code
 from datetime import datetime, timedelta
 from functools import wraps
 import jwt, os
